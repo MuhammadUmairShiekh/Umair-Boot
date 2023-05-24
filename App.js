f381bc965a1f39af6c7fa6d891dd3ae7958e2bba
MuhammadUmairShiekh <132872685+MuhammadUmairShiekh@users.noreply.github.com>
2023-05-24 10:28:03 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+// const hourElem=document.getElementById("hour");
+// const minElem=document.getElementById("min");
+// const secElem=document.getElementById("sec");
+// const amPm=document.getElementById("amPm");
+
+// setInterval(function(){
+//     const date = new Date ();
+//     let hour = date.getHours();
+//     let amPm = "Am";
+//     if ( hour > 11  ){
+//         amPm = "Pm";    
+//     if ( hour > 12  ){
+//          hour -= 12;   
+
+//     } }          
+
+//     hourElem.innerText = (hour);
+//     minElem.innerText = date.getMinutes();
+//     secElem.innerText = date.getSeconds();
+//     amPm.innerText = (amPm)
+    
+
+
+// },1000);
